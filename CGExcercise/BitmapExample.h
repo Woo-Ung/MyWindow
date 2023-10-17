b9f95e9b8e3a6815f6b12d17f68324716621f0f4
ehddnr <ehddnrdlaos@gmail.com>
2023-10-17 18:36:25 +0900

    Update/1017 CGExcercise

--- CGExcercise/BitmapExample.h
+++ CGExcercise/BitmapExample.h
@@ -4,7 +4,7 @@
 
 const int BITMAP_WIDTH = 1024;
 const int BITMAP_HEIGHT = 768;
-const int BITMAP_BYTECOUBT = 4;
+const int BITMAP_BYTECOUNT = 4;
 
 class BitmapExample : public D2DFramework
 {
@@ -12,7 +12,15 @@ private:
 	Microsoft::WRL::ComPtr<ID2D1Bitmap> mspFrameBuffer;
 	std::unique_ptr<UINT8[]> mspBackBuffer;
 
+protected:
+	virtual HRESULT CreateDeviceResources() override;
+
 public:
 	virtual HRESULT Initialize(	HINSTANCE hInstance, LPCWSTR title = L"BitmapExample", UINT w = BITMAP_WIDTH, UINT h = BITMAP_HEIGHT) override;
-	virtual void Render();
+	virtual void Render() override;
+
+	void DrawPixel(int x, int y, D2D1::ColorF color);
+	void DrawRectangle(int left, int top, int w, int h, D2D1::ColorF color);
+	void ClearBuffer(D2D1::ColorF color);
+	void PresentBuffer();
 };
