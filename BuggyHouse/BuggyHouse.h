19a621c68e257d8be0c331b6d633d3613264e2f2
ehddnr <ehddnrdlaos@gmail.com>
2023-10-25 21:06:37 +0900

    BuggyHouse

---
+++ BuggyHouse/BuggyHouse.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <list>
+#include <memory>
+#include "D2DFramework.h"
+#include "Actor.h"
+
+class BuggyHouse : public D2DFramework
+{
+private:
+	std::list<std::shared_ptr<Actor>> mBugList;
+	std::shared_ptr<Actor> mspBackground;
+
+public:
+	virtual HRESULT Initialize(HINSTANCE hInstance, LPCWSTR titile = L"BuggyHouse",
+		UINT width = 1024, UINT height = 768) override;
+	virtual void Render() override;
+	virtual void Release() override;
+};
+
