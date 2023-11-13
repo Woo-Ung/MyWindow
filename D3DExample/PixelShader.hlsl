99398cfed41e93fd278c2034c315a87e8db94d91
ehddnr <ehddnrdlaos@gmail.com>
2023-11-13 18:16:55 +0900

    Update/1113 D3DExample rectangle

--- D3DExample/PixelShader.hlsl
+++ D3DExample/PixelShader.hlsl
@@ -1,4 +1,27 @@
-float4 main(float4 position : SV_POSITION, float4 color : COLOR) : SV_TARGET
+texture2D ShaderTexture;
+
+SamplerState Sampler
 {
-    return color;
+    AddressU = Warp;
+    AddressV = Wrap;
+    Filter = MIN_MAG_MIP_LINEAR;
+};
+
+float4 main(float4 position : SV_POSITION, float2 tex: TEXCOORD0) : SV_TARGET
+{
+    float4 result = ShaderTexture.Sample(Sampler, tex);
+              
+    //result[0] = result[1] = result[2] = result[0] * 0.21f + result[1] * 0.72f + result[2] * 0.07f;  // grayscale1
+    //result[0] = result[1] = result[2] = (result[0] + result[1] + result[2]) / 3;    // grayscale2
+    
+    //if ( result[0] > 0.6f)                          // ¸ğ³ëÅæ
+    //{
+    //    result[0] = result[1] = result[2] = 1.0f;
+    //}
+    //else
+    //{
+    //    result[0] = result[1] = result[2] = 0.0f;
+    //}
+    
+    return result;
 }
